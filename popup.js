8f58dd6c804d2ffc38feabd27a1d372ab4a68c35
Jayant Ramdurg <118149520+JackSpar45@users.noreply.github.com>
2025-05-03 00:01:27 +0530

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,23 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const blockYouTubeCheckbox = document.getElementById('blockYouTube');
-    const blockLinkedInCheckbox = document.getElementById('blockLinkedIn');
-    const saveBtn = document.getElementById('saveBtn');
+  const blockYouTubeCheckbox = document.getElementById('blockYouTube');
+  const blockLinkedInCheckbox = document.getElementById('blockLinkedIn');
+  const blockRedditCheckBox = document.getElementById('blockReddit');
+  const saveBtn = document.getElementById('saveBtn');
 
-   //Load saved Preferences
-   chrome.storage.sync.get(['blockYouTube', 'blockLinkedIn'],(result) =>{
-       blockYouTubeCheckbox.checked = result.blockYouTube || false;
-       blockLinkedInCheckbox.checked = result.blockLinkedIn || false;
-   });
+ //Load saved Preferences
+ chrome.storage.sync.get(['blockYouTube', 'blockLinkedIn','blockReddit'],(result) =>{
+     blockYouTubeCheckbox.checked = result.blockYouTube || false;
+     blockLinkedInCheckbox.checked = result.blockLinkedIn || false;
+     blockRedditCheckBox.checked = result.blockReddit || false;
+ });
 
-    // Save preferences when button is clicked
-  saveBtn.addEventListener('click', () => {
-    const blockYouTube = blockYouTubeCheckbox.checked;
-    const blockLinkedIn = blockLinkedInCheckbox.checked;
-    alert('Preferences saved');
-    chrome.storage.sync.set({ blockYouTube, blockLinkedIn }, () => {
-      console.log('Preferences saved');
-    });
+  // Save preferences when button is clicked
+saveBtn.addEventListener('click', () => {
+  const blockYouTube = blockYouTubeCheckbox.checked;
+  const blockLinkedIn = blockLinkedInCheckbox.checked;
+  const blockReddit = blockRedditCheckBox.checked;
+  alert('Preferences saved');
+  chrome.storage.sync.set({ blockYouTube, blockLinkedIn, blockReddit }, () => {
+    console.log('Preferences saved');
   });
-
+});
 
 });
